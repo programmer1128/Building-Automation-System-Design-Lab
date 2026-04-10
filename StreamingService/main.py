b2a8c6ef543f8876f5e34a10667f0c4e213a8c4a
cubexgani <mroybros30@gmail.com>
2026-04-10 23:15:34 +0530

    Add logging and enable CORS for frontend access

--- StreamingService/main.py
+++ StreamingService/main.py
@@ -1,7 +1,30 @@
+import logging
+import os
+
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Allows all origins, change this to your specific IP/domain in production
+    allow_credentials=True,
+    allow_methods=["*"],  # Allows all methods (GET, POST, etc.)
+    allow_headers=["*"],
+)
+
+logging.basicConfig(
+        level=logging.INFO,
+        format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+        datefmt='%I:%M:%S %p'
+)
+
+logger = logging.getLogger(__name__)
+
+logger.info("CWD: " + os.getcwd())
+
 @app.get("/")
 def root():
+    logger.info("API endpoint / hit")
     return {"message": "Hello there!"}
