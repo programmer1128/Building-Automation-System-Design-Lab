cd70dced5d81cc59755870a17d72faacd34548b6
cubexgani <mroybros30@gmail.com>
2026-04-30 14:45:23 +0530

    Add video stream component to frontend

--- StreamingFrontend/app/(tabs)/index.tsx
+++ StreamingFrontend/app/(tabs)/index.tsx
@@ -1,8 +1,10 @@
-import { StyleSheet, useColorScheme } from 'react-native';
+import { StyleSheet, useColorScheme, View } from 'react-native';
 
+import { ThemedText } from '@/components/themed-text';
+import { ThemedView } from '@/components/themed-view';
+import VideoStream from '@/components/video-stream';
 import { Picker } from '@react-native-picker/picker';
 import { useState } from 'react';
-import { View } from 'react-native';
 
 type CameraID = 'camera1' | 'camera2';
 
@@ -11,25 +13,33 @@ export default function HomeScreen() {
   const theme = useColorScheme() ?? 'light';
 
   return (
-    <View>
-       <Picker
-            selectedValue={camId}
-            onValueChange={(itemValue) => {
-              setCamId(itemValue as CameraID);
-              console.log("Camera id", itemValue)
-            }}
-            style={styles.picker}
-            itemStyle={styles.pickertext}
-            dropdownIconColor={theme === 'dark' ? '#fff' : '#000'}
-          >
-            <Picker.Item label="Living Room" value="camera1" />
-            <Picker.Item label="Bedroom" value="camera2" />
+    <ThemedView style={styles.container}>
+      <ThemedText type="title">Stream</ThemedText>
+      <VideoStream camId={camId} />
+       <View style={styles.controls}>
+          <Picker
+              selectedValue={camId}
+              onValueChange={(itemValue) => {
+                setCamId(itemValue as CameraID);
+                console.log("Camera id", itemValue)
+              }}
+              style={styles.picker}
+              itemStyle={styles.pickertext}
+              dropdownIconColor={theme === 'dark' ? '#fff' : '#000'}
+            >
+              <Picker.Item label="Living Room" value="camera1" />
+              <Picker.Item label="Bedroom" value="camera2" />
           </Picker>
-    </View>
+          </View>
+    </ThemedView>
   );
 }
 
 const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingTop: 60,
+  },  
   titleContainer: {
     flexDirection: 'row',
     alignItems: 'center',
@@ -52,5 +62,9 @@ const styles = StyleSheet.create({
   },
   pickertext: {
     color: 'white'
-  }
+  },
+  controls: {
+    padding: 24,
+    gap: 12,
+  },
 });
